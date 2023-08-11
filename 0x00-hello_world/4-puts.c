04668eb71a8f11d838973d3328bc6026cc6e1513
Youssef-Azerg <youssefazerg2002@gmail.com>
2023-08-10 18:17:26 -0700

    My commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
-int main(void){
+
+/**
+ * main - entry point
+ *
+ * Description: C program that prints with put function
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
